d15d0eabb87d895d0c1fd23185675e62e1573ab2
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-20 23:10:25 +0100

    Formattage job09

--- jour01/job09/main.c
+++ jour01/job09/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    char* nom = "Pain au chocolat";
+int main()
+{
+    char *nom = "Pain au chocolat";
     float prix_unitaire = 1;
     int quantite_stock = 20;
-    printf("%s - Prix: %.2f euros ; Qte.: %i\n",nom,prix_unitaire,quantite_stock);
+    printf("%s - Prix: %.2f euros ; Qte.: %i\n", nom, prix_unitaire, quantite_stock);
     int quantite_souhaite;
     printf("Combien de %s souhaitez vous acheter? : ", nom);
     scanf("%d", &quantite_souhaite);
-    if (quantite_souhaite <= quantite_stock) {
+    if (quantite_souhaite <= quantite_stock)
+    {
         quantite_stock -= quantite_souhaite;
         printf("Vous devez payer %.2f euros\n", quantite_souhaite * prix_unitaire);
-    } else {
+    }
+    else
+    {
         printf("Nous sommes desoles, nous n'avons pas assez de stock\n");
     }
-   
+
     printf("%s - Prix: %.2f euros ; Qte.: %i\n", nom, prix_unitaire, quantite_stock);
 }
