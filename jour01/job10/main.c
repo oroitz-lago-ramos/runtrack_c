ac00b11b25390e626c7eb239ca75203b5c9e3718
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-11-21 00:12:50 +0100

    Ajout du job10

--- jour01/job10/main.c
+++ jour01/job10/main.c
@@ -11,7 +11,7 @@ int main()
     gain = montant_initial * taux_annuel / 100;
     printf("Si le capital initial augmente de 5000 euros, le gain au bout d'une annee sera : %.2f euros\n", gain);
     float montant_total = montant_initial + gain;
-    printf("Suposons maintenant que l'utilisateur veut retirer 10%c de son montant total au bout d'un an qui est de  %.2f euros\n", "%", montant_total);
+    printf("Suposons maintenant que l'utilisateur veut retirer 10%c de son montant total au bout d'un an qui est de %.2f euros\n", '%', montant_total);
     montant_total -= montant_total * 10 / 100;
     montant_initial = montant_total;
     taux_annuel -= 1;
